c498cf9288576a48e3a4224f96f48342a2010bd5
Jatin Sonani <163812641+JatinSonani@users.noreply.github.com>
2024-05-04 23:24:51 +0530

    Add files via upload

---
+++ Q-1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    char password[50];
+    int i, has_letter = 0, has_digit = 0, has_special = 0;
+
+    printf("Create your password: ");
+    scanf("%s", password);
+
+    if (strlen(password) < 6)
+	{
+        printf("Your password is not Strong. It should be at least 6 characters long.\n");
+        return 0;
+    }
+
+    for (i = 0; i < strlen(password); i++)
+	{
+        if (isalpha(password[i])) {
+            has_letter = 1;
+        } else if (isdigit(password[i])) {
+            has_digit = 1;
+        } else if (password[i] == '@' || password[i] == '$' || password[i] == '!' || password[i] == '%' || password[i] == '*' || password[i] == '?' || password[i] == '&') {
+            has_special = 1;
+        }
+    }
+
+    if (has_letter && has_digit && has_special) {
+        printf("Your password is Strong.\n");
+    } else {
+        printf("Your password is not Strong. It must contain at least one letter, one digit, and one special symbol.\n");
+    }
+}
---
+++ Q-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char email[50] = "admin@gmail.com";
+    char password[20] = "123456";
+    char input_email[50];
+    char input_password[20];
+
+    printf("Enter your email: ");
+    scanf("%s", input_email);
+
+    printf("Enter your password: ");
+    scanf("%s", input_password);
+
+    if (strcmp(email, input_email) == 0 && strcmp(password, input_password) == 0)
+	{
+        printf("Login Successful...\n");
+    } else {
+        printf("Login Failed. Invalid Credentials.\n");
+    }
+}

cb5e74121d468a5364157bac18100ed5154cd5c0
Jatin Sonani <163812641+JatinSonani@users.noreply.github.com>
2024-05-04 23:27:22 +0530

    Q-2.c

--- Q-2.c
+++ Q-2.c
@@ -1,3 +1,6 @@
+// Q.2 Write a Program to create a login mechanism by entering the right email & password. 
+// 	You can compare it with your desired email & password.
+
 #include <stdio.h>
 #include <string.h>
 
